aa72e83f7bb8d86692d353877b123a04062ebee0
Josh Borrow <josh@joshborrow.com>
2024-05-10 11:43:48 -0400

    Use async for

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ class WebsocketRequest(BaseModel):
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    # The FastAPI tutorial will tell you to use while True: here.
+    # That's not a good idea, as it can block the even loop. Use
+    # the async for over the text iteration instead.
     await websocket.accept()
     try:
-        while True:
-            data = await websocket.receive_text()
-
-            formatted = WebsocketRequest.parse_raw(data)
+        async for message in websocket.iter_text():
+            formatted = WebsocketRequest.parse_raw(message)
 
             print(f"Data received: {formatted}")
 
